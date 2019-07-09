83a2c806e170dcd3b0bd7f8572a31e6c8cccaf96
manandearth <adamgefen@posteo.net>
2019-07-09 16:04:59 +0200

    Write redux boiler plate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { addRecipe } from './redux/reducers/index.js';
+
+function ColorBtn() {
+    return (
+<button>cook</button>
+    );
+
+} ;
+
+class NewRecipe extends React.Component {
+
+    constructor (props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className='new-recipe'>
+              <h2>Add a new recipe</h2>
+              <p>Title -&gt; <input></input></p>
+              <input className='ingredients-input'></input>
+              <div className='ingredient-list'>here comes list</div>              
+            </div>
+        );
+    }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ColorBtn />
+        <h2 className='App-title'>Weekly shopping</h2>
       </header>
+      <NewRecipe />
     </div>
   );
 }
