426c1ec4fadd8dabb6996ece7ca66e5a56db11fc
manandearth <adamgefen@posteo.net>
2019-07-23 16:47:18 +0200

    Updates

---
+++ precache-manifest.3f921caff6b5329371bad03e1515944d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08384236e7b0ee4d53a7ad7ca6d49c4",
+    "url": "/weekly-shopping/index.html"
+  },
+  {
+    "revision": "784add8afdeb6baa3308",
+    "url": "/weekly-shopping/static/css/main.d400a515.chunk.css"
+  },
+  {
+    "revision": "9e289f1abf984766a580",
+    "url": "/weekly-shopping/static/js/2.451f10a4.chunk.js"
+  },
+  {
+    "revision": "784add8afdeb6baa3308",
+    "url": "/weekly-shopping/static/js/main.59bebbf9.chunk.js"
+  },
+  {
+    "revision": "3129f8e40db2010126de",
+    "url": "/weekly-shopping/static/js/runtime~main.e6f136e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weekly-shopping/static/media/logo.5d5d9eef.svg"
+  }
+]);
