cce8c6b7307d66e77c90c941bf952e6c4abe620d
manandearth <adamgefen@posteo.net>
2019-07-26 11:52:20 +0200

    Add redux-persist to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,24 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+
+const persistConfig = {
+ key: 'root',
+ storage: storage,
+ stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(pReducer);
+export const persistor = persistStore(store);
+
+
+
+//ORIGIAL STORE SETUP
+// export default createStore(rootReducer,
+// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
