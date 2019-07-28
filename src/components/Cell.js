9d6d635cf06f1514e110a18b87e3c4536e1dee26
manandearth <adamgefen@posteo.net>
2019-07-28 15:41:55 +0200

    Add handles for onClick in 'Cell' and add state

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,16 +1,51 @@
 import React from 'react';
+import './Cell.css';
 
 class Cell extends React.Component {
 
-	render () {
+  constructor(props){
+    super(props);
+    this.state = {
+      editable: false,
+      dish:'',
+      servings: 0
+    };
+    
+  }
+  
+  handleAddMeal = () => {
+    this.setState({editable: !this.state.editable});
+  }
+
+  handleRemoveMeal = () => {
+    this.setState({editable: !this.state.editable});
+  }
+  
+  render () {
 		return(
       <div className='cell'>
-        <h2>Recipe:</h2>
-        <h2>Servings:</h2>
-      </div>
 
-		);
-	}
+        {this.state.editable === false
+          ? 
+          <p
+            className='plus'
+           onClick={this.handleAddMeal}
+          >+</p>
+          :
+          <div>
+            <span
+              className='cross'
+              onClick={this.handleRemoveMeal}
+            >x</span>
+            <h2>Dish:<input></input></h2>
+            <h2>Servings:<input></input></h2></div>
+          
+        }
+      </div>
+    );
+  }
 }
+	
+
 
 export default Cell;
